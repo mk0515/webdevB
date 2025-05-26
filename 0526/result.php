dd5a64d62c2652360b05b852c62aa1c52668875e
mk0515 <mika.koumura.0312@gmail.com>
2025-05-26 16:54:22 +0900

    0526

---
+++ 0526/result.php
@@ -0,0 +1,7 @@
+<?php
+// register.php
+// POSTで送信された値を受け取る
+echo "<h1>受け取ったデータ</h1>";
+echo $_POST["name"] . "さん、こんにちは。";
+echo "あなたの年齢は" . $_POST["age"] . "歳ですね";
+var_dump($_POST);
---
+++ 0526/weight.php
@@ -0,0 +1,20 @@
+<?php
+require_once 'function.php';
+
+$height = (float) $_POST['height'];
+$weight = (float)$_POST['weight'];
+$goal_weight = $height * $height * 22;
+$difference = abs($goal_weight - $weight);
+
+if ((($height <= 0) || (3 <= $height))) {
+  echo "正しい身長を入力してください";
+  exit;
+}
+if ((($weight < 0) || (200 < $weight))) {
+  echo "正しい体重を入力してください";
+  exit;
+};
+
+echo "体重" . $weight . "kg<br>";
+echo "理想" . $goal_weight . "kg<br>";
+echo "後" . $difference . "kgで適正体重です";
